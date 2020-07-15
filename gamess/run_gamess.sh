ba6deb7150b664cc94cf217e22454c3380fae652
Yasuhisa HONDA <nqvno14@gmail.com>
2020-07-16 01:19:48 +0900

    Dockerfile & shellscript for gamess

---
+++ gamess/run_gamess.sh
@@ -0,0 +1,1 @@
+docker run --gpus all -itd --expose 22 -p 127.0.0.1:30000:22 -p 127.0.0.1:30001:8888 -p 127.0.0.1:30002:8889 -p 127.0.0.1:30003:6006 --hostname hondaya-gamess --name hondaya-gamess -v /net/fs09/work01/hondaya:/net/fs09/work02/hondaya -v /net/fs10/work01/hondaya:/net/fs10/work01/hondaya -v /net/fs09/homes/people:/net/fs09/homes/people hondaya/gamess:1.0
